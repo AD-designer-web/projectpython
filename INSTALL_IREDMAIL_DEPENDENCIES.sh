ea52cf3bbba95c89065f6dad7e8d6885aae58d90
Abdul_Djafar <76455540+AD-designer-web@users.noreply.github.com>
2022-04-04 01:06:38 +0000

    Add files via upload

---
+++ INSTALL_IREDMAIL_DEPENDENCIES.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+wget https://github.com/iredmail/iRedMail/archive/refs/tags/1.5.2.tar.gz
+cd /home/nabou/Bureau
+tar -xf 1.5.2.tar.gz
+cd /home/nabou/Bureau/iRedMail-1.5.2
+chmod +x iRedMail.sh
+bash iRedMail.sh
+
+
+
+
+
+
